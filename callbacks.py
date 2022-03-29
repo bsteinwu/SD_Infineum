a267a8d5472a9aa87efe0e0a41a663c88c4540ec
cmcalder55 <67109992+cmcalder55@users.noreply.github.com>
2022-03-29 16:10:22 -0400

    Add files via upload

---
+++ callbacks.py
@@ -0,0 +1,142 @@
+import dash
+from dash.dependencies import Output, Input
+from openpyxl import load_workbook, Workbook
+#import dash_core_components as dcc
+from dash import dcc
+#import dash_html_components as html
+from dash import html
+import os.path
+
+n = 0
+
+slots = 45    
+shelf = []
+empty_color = 'white'
+full_color = 'black'
+
+for i in range(slots+1):                     #create shelf colors dictionary
+    slot_name = 'Slot_'+str(i)+'_Color'
+    shelf.append(slot_name)
+    Shelf_Colors = dict.fromkeys(shelf, empty_color)
+
+book_name = 'test1.xlsx'
+file_exists = os.path.exists(book_name)
+
+def create_workbook(path):
+    '''Create new empty Excel workbook in current directory if it does not exist.'''
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet["A1"] = "Location Number"
+    sheet["B1"] = "ELN Number"
+    sheet["C1"] = "Arrival Date"
+    sheet["D1"] = "Expiration Date"
+    workbook.save(path)
+    
+if file_exists:           #check if Excel file exists in current path
+    print('')
+    print(f'The file {book_name} exists.')
+else:
+    print('')
+    print(f'The file {book_name} does not exist; creating new workbook.') 
+    create_workbook(book_name)
+    
+    
+wb = load_workbook('Shelf Data.xlsx')    
+ws = wb['Shelf 1']
+
+## updates what is currently selected
+
+for i in range(2,47):
+    if ws.cell(row = i, column = 2).value is None:
+        continue
+    Shelf_Colors['Slot_' + str(i-1) + '_Color'] = full_color           # set color for filled slots
+
+def register(app:dash):
+    @app.callback(Output('sample_info', 'children'),
+              [Input('Slot_1', 'n_clicks'),
+              Input('Slot_2', 'n_clicks'),
+              Input('Slot_3', 'n_clicks'),
+              Input('Slot_4', 'n_clicks'),
+              Input('Slot_5', 'n_clicks'),
+              Input('Slot_6', 'n_clicks'),
+              Input('Slot_7', 'n_clicks'),
+              Input('Slot_8', 'n_clicks'),
+              Input('Slot_9', 'n_clicks'),
+              Input('Slot_10', 'n_clicks'),
+              Input('Slot_11', 'n_clicks'),
+              Input('Slot_12', 'n_clicks'),
+              Input('Slot_13', 'n_clicks'),
+              Input('Slot_14', 'n_clicks'),
+              Input('Slot_15', 'n_clicks'),
+              Input('Slot_16', 'n_clicks'),
+              Input('Slot_17', 'n_clicks'),
+              Input('Slot_18', 'n_clicks'),
+              Input('Slot_19', 'n_clicks'),
+              Input('Slot_20', 'n_clicks'),
+              Input('Slot_21', 'n_clicks'),
+              Input('Slot_22', 'n_clicks'),
+              Input('Slot_23', 'n_clicks'),
+              Input('Slot_24', 'n_clicks'),
+              Input('Slot_25', 'n_clicks'),
+              Input('Slot_26', 'n_clicks'),
+              Input('Slot_27', 'n_clicks'),
+              Input('Slot_28', 'n_clicks'),
+              Input('Slot_29', 'n_clicks'),
+              Input('Slot_30', 'n_clicks'),
+              Input('Slot_31', 'n_clicks'),
+              Input('Slot_32', 'n_clicks'),
+              Input('Slot_33', 'n_clicks'),
+              Input('Slot_34', 'n_clicks'),
+              Input('Slot_35', 'n_clicks'),
+              Input('Slot_36', 'n_clicks'),
+              Input('Slot_37', 'n_clicks'),
+              Input('Slot_38', 'n_clicks'),
+              Input('Slot_39', 'n_clicks'),
+              Input('Slot_40', 'n_clicks'),
+              Input('Slot_41', 'n_clicks'),
+              Input('Slot_42', 'n_clicks'),
+              Input('Slot_43', 'n_clicks'),
+              Input('Slot_44', 'n_clicks'),
+              Input('Slot_45', 'n_clicks'),
+              ])
+    def Display_Shelves(
+    Slot_1,Slot_2,Slot_3,Slot_4,Slot_5,Slot_6,Slot_7,Slot_8,Slot_9,Slot_10,
+    Slot_11,Slot_12,Slot_13,Slot_14,Slot_15,Slot_16,Slot_17,Slot_18,Slot_19,Slot_20,
+    Slot_21,Slot_22,Slot_23,Slot_24,Slot_25,Slot_26,Slot_27,Slot_28,Slot_29,Slot_30,
+    Slot_31,Slot_32,Slot_33,Slot_34,Slot_35,Slot_36,Slot_37,Slot_38,Slot_39,Slot_40,
+    Slot_41,Slot_42,Slot_43,Slot_44,Slot_45):
+        global n
+        update_sample_info = dash.callback_context
+        current_sample = update_sample_info.triggered[0]['prop_id'].split('.')[0]
+
+        if n == 0:
+            ELN_number = 'No sample selected'
+            Arrial_date = ''
+            experation_date =''
+            n = n + 1
+        else:
+            for i in range(2,47):
+                if current_sample == 'Slot_' + str(i-1):
+                    if Shelf_Colors['Slot_' + str(i-1) + '_Color'] == empty_color:
+                        ELN_number = str(ws.cell(row = i, column = 2).value)
+                        Arrial_date = str(ws.cell(row = i, column = 3).value)[:-9]
+                        experation_date = str(ws.cell(row = i, column = 4).value)[:-9]
+                    else:
+                        ELN_number = 'No sample found'
+                        Arrial_date = ''
+                        experation_date =''
+
+        return html.Div([
+                html.Table([
+                    html.Tr([
+                        html.Th(str(ws.cell(row = 1, column = 2).value)),
+                        html.Th(str(ws.cell(row = 1, column = 3).value)),
+                        html.Th(str(ws.cell(row = 1, column =4).value)),
+                    ]),
+                    html.Tr([
+                        html.Th(ELN_number),
+                        html.Th(Arrial_date),
+                        html.Th(experation_date),
+                    ]),
+                ], style={'Boarder':'solid', 'text-align':'center', 'font-size':'20px', 'margin-left': 'auto', 'margin-right' : 'auto'})
+            ], style={'vertical-align':'top'})
